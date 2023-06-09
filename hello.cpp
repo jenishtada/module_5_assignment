219e0b6c70955e628623765ad46aed2adb12e3f1
jenishtada <jenishtada3425@gmail.com>
2023-06-09 11:16:56 +0530

    module 5 complated

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // printf("Hello world");
+
+    cout << "Hello World" << endl;
+    cout << "Welcome c plus plus programming" << endl;
+    cout << "Welcome to tops technologies..";
+}
